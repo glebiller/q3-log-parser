bd8a0cf8fb47fd8ea90e2ac99f18f40a7c651be5
Guillaume Le Biller <lebiller@fullsix.com>
2013-05-30 11:14:02 +0200

    Compute hash based on a sorted list of players.

--- src/main/java/fr/kissy/q3logparser/funnel/GameFunnel.java
+++ src/main/java/fr/kissy/q3logparser/funnel/GameFunnel.java
@@ -1,7 +1,10 @@
 package fr.kissy.q3logparser.funnel;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Ordering;
 import com.google.common.hash.Funnel;
 import com.google.common.hash.PrimitiveSink;
+import com.sun.javafx.collections.transformation.SortedList;
 import fr.kissy.q3logparser.dto.Game;
 import fr.kissy.q3logparser.dto.Player;
 
@@ -16,7 +19,8 @@ public class GameFunnel implements Funnel<Game> {
         into.putString(game.getType().name())
             .putString(game.getMap())
             .putInt(game.getDuration());
-        for (Player player : game.getPlayers().values()) {
+        ImmutableList<Player> sortedPlayers = Ordering.natural().immutableSortedCopy(game.getPlayers().values());
+        for (Player player : sortedPlayers) {
             into.putString(player.getTeam().name())
                 .putString(player.getName())
                 .putInt(player.getScore())
